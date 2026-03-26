b4e1655de2be0cc3e5534b36dd7ee22dadc2e76e
Divyansh Sanadhya <ds3038@srmist.edu.in>
2026-03-26 10:36:44 +0530

    UC1: Implemented Hello World using standard Java

---
+++ pt2/HelloApp.java
@@ -0,0 +1,17 @@
+/**
+ * HelloApp.java - A simple Java application that Displays "Hello, World!" to
+ * the console.
+ *
+ * This application demonstrates fundamental Java concepts such as 
+ * class declaration, the main method, and standard output.
+ *
+ * @author Divyansh Sanadhya
+ * @version 1.0
+ */
+
+public class HelloApp {
+    public static void main(String[] args) {
+        // Standard output stream to print the message
+        System.out.println("Hello World");
+    }
+}
